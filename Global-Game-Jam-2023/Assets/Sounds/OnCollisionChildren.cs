a9a1695175c313415ea4c691e61cf72fdaf4ada9
Lme20 <Louisme.2014@gmail.com>
2023-02-05 12:42:49 +0100

    Pushing again

---
+++ Global-Game-Jam-2023/Assets/Sounds/OnCollisionChildren.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip clip;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //find component on game object
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip; //can change sound 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     //When player enters collision 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //REPLACE "Player" with actual PlayerOject
+        if (collision.gameObject.name.Equals("Character"))
+        {
+            //Interact allowed becomes true = object active
+            PlaySound();
+        }
+    }
+  
+    //When player exits collision
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //REPLACE "Player" with actual PlayerOject
+        if (collision.gameObject.name.Equals("Character"))
+        {
+            //InteractAllowed becomes false = object NOT active
+             audioSource.enabled = !audioSource.enabled;
+
+
+        }
+
+    }
+
+
+    //Play sound
+    private void PlaySound()
+    {
+    
+    //Could probably insert MiniGame trigger here?
+
+    //Placeholder code
+
+        audioSource.Play();
+
+    }
+}
