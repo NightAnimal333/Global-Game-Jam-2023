c67185c1e298ea4ddf3e36063f77cde45038de38
NightAnimal <artursu@stud.ntnu.no>
2023-02-05 15:22:16 +0100

    Title update

---
+++ Global-Game-Jam-2023/Assets/titleScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleScript : MonoBehaviour
+{
+
+    public GameObject letterCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("space"))
+        {
+
+            letterCanvas.SetActive(true);
+            Destroy(gameObject);
+
+        }
+
+        
+    }
+}
